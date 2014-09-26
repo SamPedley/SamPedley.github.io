a694aaf9d28332005e474d5a8c6836582bd79e23
Sam <sjpeds@gmail.com>
2014-09-26 16:12:59 -0500

    added old site content

---
+++ samPedley/_src/_js/global.js
@@ -0,0 +1,6 @@
+console.log('"Say Hello to the Cat"');
+console.log('      |\\---/|');
+console.log('      | o_o |');
+console.log('       \\_^_/');
+
+var Hello = 'Glad to see you can use the comand line. Intrested in hiring a web devloper or want to colaborate type work';
