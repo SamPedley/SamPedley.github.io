902d307124953173e9575cd3539e462d071fc2b1
sampedley <sjpeds@gmail.com>
2015-04-17 20:04:11 -0500

    updated posts

---
+++ _site/Gruntfile.js
@@ -0,0 +1,114 @@
+  "use strict";
+
+module.exports = function(grunt) {
+
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON("package.json"),
+
+    sass: {
+      global: {
+        options: {
+          style: "compressed",
+          banner: '/*! <%= grunt.template.today("yyyy-mm-dd") %> */'
+        },
+        files: {
+          "dev/F/css/main-unprefixed.css": "dev/_src/_scss/main.scss"
+        }
+      }
+    },
+
+    autoprefixer: {
+      global: {
+        src: "dev/F/css/main-unprefixed.css",
+        dest: "dev/F/css/main.css"
+      }
+    },
+
+    uglify: {
+      options: {
+        banner: '/*! <%= grunt.template.today("yyyy-mm-dd") %> */'
+      },
+      my_target: {
+        files: {
+          'dev/F/js/global.js': ['dev/_src/_js/global.js'],
+          'dev/F/js/jQuery_fitVids.js': ['dev/_src/_js/jquery.js','dev/_src/_js/fitVids.js']
+        }
+      }
+    },
+
+    imagemin: { 
+      dynamic: {                         // Another target
+        files: [{
+          expand: true,                  // Enable dynamic expansion
+          cwd: 'dev/_src/_img/',                   // Src matches are relative to this path
+          src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match
+          dest: 'dev/F/img'                  // Destination path prefix
+        }]
+      }
+    },
+    
+    shell: {
+       jekyllServe: {
+        command: [
+                'cd dev',
+                'jekyll serve --baseurl=',
+            ].join('&&')
+       },
+      jekyllBuild: {
+
+         command: [
+                'cd dev',
+                'jekyll build --config _config.yml',
+            ].join('&&')
+      }
+    },
+    watch: {
+      options: {
+        livereload: true
+      },
+      site: {
+        files: ["dev/_layouts/**/*.html",'dev/_posts/*.markdown'],
+        tasks: ["shell:jekyllBuild"],
+        livereload: true
+      },
+      css: {
+        files: ["dev/_src/_scss/**/*.scss",],
+        tasks: ["sass", "autoprefixer", "shell:jekyllBuild"]
+      },
+      svg: {
+        files: ["dev/_src/_svg/*.svg"],
+        tasks: ["svgstore", "shell:jekyllBuild"]
+      },
+      js: {
+        files: ["dev/_src/_js/*.js"],
+        tasks: ["uglify", "shell:jekyllBuild"]
+      }
+    },
+
+    svgstore: {
+      options: {
+        prefix : "shape-",
+        cleanup: false,
+        svg: {
+          style: "display: none;"
+        }
+      },
+      default: {
+        files: {
+          "dev/_includes/svg-defs.svg": ["dev/_src/_svg/*.svg"]
+        }
+      }
+    }
+
+  });
+
+  require('load-grunt-tasks')(grunt);
+
+  grunt.registerTask("serve", ["shell:jekyllServe"]);
+  grunt.registerTask("img", ["imagemin"]);
+  grunt.registerTask("build", ["sass", "autoprefixer","uglify", "svgstore","shell:jekyllBuild"]);
+  grunt.registerTask("default", ["sass", "autoprefixer","uglify", "svgstore", "shell:jekyllBuild","watch"]);
+
+
+};
---
+++ dev/_posts/2015-04-16-front-end-developer-tests.markdown
@@ -0,0 +1,40 @@
+---
+layout: post
+title:  "Junior Front-End Questions"
+date:   2015-4-16 13:58:00
+categories: 
+published: true
+description:  
+image: /F/img/terminalThemes/flatland.png
+---
+
+Interviewing is not easy.  Some amazing people interview terribly and some unqualified cannidates might know just what to say but just can't perform the way the job might need.  Each industry has to take into account ways to test possible emploies and the best part about development is that we can quickly and eaisly test them. 
+
+
+##Responsive test
+
+Out of the three this is thte test I would give the most weight. Taking a set of divs and styling them in a way that flows well between screen sizes is exactly what these developers will be doing on a daily basis and a strong base understanding of these concepts is very important. 
+
+<p data-height="350" data-theme-id="0" data-slug-hash="VYNeOO" data-default-tab="result" data-user="SamPedley" class='codepen'>See the Pen <a href='http://codepen.io/SamPedley/pen/VYNeOO/'>Display Issue: Question</a> by SamPedley (<a href='http://codepen.io/SamPedley'>@SamPedley</a>) on <a href='http://codepen.io'>CodePen</a>.</p>
+<script async src="//assets.codepen.io/assets/embed/ei.js"></script>
+
+-------------------
+
+##Practical test
+
+jQuery is 
+
+<p data-height="350" data-theme-id="0" data-slug-hash="MYRyBP" data-default-tab="result" data-user="SamPedley" class='codepen'>See the Pen <a href='http://codepen.io/SamPedley/pen/MYRyBP/'>jQuery Test: Question</a> by SamPedley (<a href='http://codepen.io/SamPedley'>@SamPedley</a>) on <a href='http://codepen.io'>CodePen</a>.</p>
+<script async src="//assets.codepen.io/assets/embed/ei.js"></script>
+
+[See my solution here.](http://codepen.io/SamPedley/pen/wBLjBx)
+
+
+-------------------
+
+##Programatic test
+
+
+
+<p data-height="350" data-theme-id="0" data-slug-hash="ZYZQPv" data-default-tab="result" data-user="SamPedley" class='codepen'>See the Pen <a href='http://codepen.io/SamPedley/pen/ZYZQPv/'>Fizz Buzz: Question</a> by SamPedley (<a href='http://codepen.io/SamPedley'>@SamPedley</a>) on <a href='http://codepen.io'>CodePen</a>.</p>
+<script async src="//assets.codepen.io/assets/embed/ei.js"></script>
